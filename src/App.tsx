847e228a18785761662e5ec6d77b145d8687b454
Erika <pierreerika@MacBook-Pro-de-Erika.local>
2023-11-20 12:18:30 +0100

    Chakra ui config & responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
-      <h1>Game Dev</h1>
+    <Grid templateAreas={{
+      base:`"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+      }}>
+      <GridItem area="nav" bg="coral">NAV</GridItem>
+      <Show above="lg">
+
+        
+        <GridItem area="aside" bg="gold">ASIDE</GridItem>
+        <GridItem area="main" bg="dodgerblue">MAIN</GridItem>
+
+      </Show>
+    </Grid>
     </>
   );
 }
