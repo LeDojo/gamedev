2c2271f943271bae0f211f1a095da967c60d38a2
J8T <jul.milcent@gmail.com>
2024-03-04 15:49:05 +0100

    :sparkles: Close RAT-61 - Color switch mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/game-analytics-icon.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
       <Text>NavBar</Text>
     </HStack>
   );
