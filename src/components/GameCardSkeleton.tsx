bf0bed80ed19f4c3125df1929855262793f6e520
J8T <jul.milcent@gmail.com>
2024-03-18 14:50:13 +0100

    Close RAT-66
    Add GameCardContainer

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 export default function GameCardSkeleton() {
   return (
-    <Card w="250px" borderRadius={10} overflow={"hidden"}>
+    <Card>
       <Skeleton h="200px" />
       <CardBody>
         <SkeletonText />
