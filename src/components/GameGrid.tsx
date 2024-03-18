77abbd1cbaed54a1cdf0e0c6721bdc5d0d1135b3
J8T <jul.milcent@gmail.com>
2024-03-18 15:49:12 +0100

    Fix: multiples bugs

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,8 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 export const GameGrid = () => {
-  const { error, games } = useGames();
+  const { error, games, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <div>
       {error && <Text>{error}</Text>}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,17 +23,26 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
+    setIsLoading(true);
     const controller = new AbortController();
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => {
         console.log(res);
-        setGames(res.data.results);
+        {
+          setGames(res.data.results);
+          setIsLoading(false);
+        }
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message);
+        {
+          setError(err.message);
+          setIsLoading(false);
+        }
       });
     return () => controller.abort();
   }, []);
